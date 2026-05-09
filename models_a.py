f0198cba24e4105103085ea104ea636e23e5a4e8
Edin Hodzic <hole@192.168.1.19>
2026-05-09 11:57:07 +0200

    Definisana pocetna struktura tabele u bazi i sheme za razmjenu podataka

--- models_a.py
+++ models_a.py
@@ -3,3 +3,24 @@ from typing import Optional
 
 # TODO: Student A - Definiši svoj SQLModel entitet ovdje
 # 
+
+//osnova struktura SQLModel entiteta
+class ArrangementBase(SQLModel):
+    destination:                    str     
+    price:                          float
+    duration_days:                  int
+    is_active:                      bool
+    description:                    Optional[str] = None
+
+class Arrangement(ArrangementBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class ArrangementCreate(ArrangementBase):
+    pass
+
+class ArrangementUpdate(SQLModel):
+    destination:                    Optional[str] = None
+    price:                          Optional[float] = None
+    duration_days:                  Optional[int] = None
+    is_active:                      Optional[bool] = None
+    description:                    Optional[str] = None
